c6557c1c4776a0ba6368618dcb03731e8a26e9f7
Félix Alejandro Guzmán <code@felixalejandro.com>
2024-08-12 12:18:38 -0400

    soften requirements

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ setup(
     author_email='felixg@clickping.do',
     url='https://github.com/Global-B/draft_mail',
     install_requires=[
-        'azure-identity==1.17.1',
-        'msgraph-sdk==1.5.4',
-        'typing-extensions==72.1.0',
-        'httpx==0.27.0',
+        'azure-identity>=1.17.1',
+        'msgraph-sdk>=1.5.4',
+        'typing-extensions>4.12.0',
+        'httpx>=0.27.0',
     ],
     classifiers=[
         'Environment :: Web Environment',
